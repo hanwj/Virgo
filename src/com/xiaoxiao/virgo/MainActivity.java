65b85f778e915a9032260f358e2afd84000671ce
hanwj <hanwj>
2015-10-19 20:38:04 +0800

    fixbug

--- src/com/xiaoxiao/virgo/MainActivity.java
+++ src/com/xiaoxiao/virgo/MainActivity.java
@@ -1,61 +1,37 @@
 package com.xiaoxiao.virgo;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.RadioGroup;
 
 public class MainActivity extends FragmentActivity {
 
-	private ArrayList<View> viewList;
+	private List<Fragment> pageList;
+    private RadioGroup mRadioGroup;
     
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_back2);
         
-        //initView();
+        mRadioGroup = (RadioGroup)findViewById(R.id.id_radiogroup);
+        pageList = new ArrayList<Fragment>();
+        
+        
+        initView();
     }
     
     private void initView(){
     	ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
-    	LayoutInflater lf = getLayoutInflater().from(this);
-    	View view1 = lf.inflate(R.layout.layout1,null);
     	
-    	viewList = new ArrayList<View>();
-    	viewList.add(view1);
-    	
-    	PagerAdapter pagerAdapter = new PagerAdapter(){
-
-			@Override
-			public int getCount() {
-				// TODO Auto-generated method stub
-				return viewList.size();
-			}
-
-			@Override
-			public boolean isViewFromObject(View arg0, Object arg1) {
-				// TODO Auto-generated method stub
-				return arg0 == arg1;
-			}
-
-			@Override
-			public Object instantiateItem(ViewGroup container, int position) {
-				// TODO Auto-generated method stub
-				return viewList.get(position);
-			}
-    		
-    	};
-    	viewPager.setAdapter(pagerAdapter);
     }
 }
