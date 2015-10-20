7c576050ae364dbeb7bd2aebc7f512c626cd8f64
hanwj <hanwj>
2015-10-20 20:44:29 +0800

    fixbug

--- src/com/xiaoxiao/fragment/HomePageFragment.java
+++ src/com/xiaoxiao/fragment/HomePageFragment.java
@@ -1,7 +1,22 @@
 package com.xiaoxiao.fragment;
 
+import com.xiaoxiao.virgo.R;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class HomePageFragment extends Fragment {
 
+	@Override
+	@Nullable
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.fragment_home_page, container, false);
+	}
+	
 }
--- src/com/xiaoxiao/virgo/MainActivity.java
+++ src/com/xiaoxiao/virgo/MainActivity.java
@@ -3,6 +3,9 @@ package com.xiaoxiao.virgo;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.xiaoxiao.fragment.HomePageFragment;
+import com.xiaoxiao.fragment.MyFragmentAdapter;
+
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.app.Fragment;
@@ -21,13 +24,15 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main_back2);
+        setContentView(R.layout.activity_main);
         
         mRadioGroup = (RadioGroup)findViewById(R.id.id_radiogroup);
         pageList = new ArrayList<Fragment>();
+        pageList.add(new HomePageFragment());
         
-        
-        initView();
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+        viewPager.setAdapter(new MyFragmentAdapter(getSupportFragmentManager(),pageList));
+        //initView();
     }
     
     private void initView(){
