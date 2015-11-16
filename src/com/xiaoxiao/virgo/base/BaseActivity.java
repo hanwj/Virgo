f96f2d232577bb649b2140abf062e300ce02176d
hanwj <hanwj>
2015-11-16 14:43:27 +0800

    util

--- src/com/xiaoxiao/virgo/base/BaseActivity.java
+++ src/com/xiaoxiao/virgo/base/BaseActivity.java
@@ -1,7 +1,21 @@
 package com.xiaoxiao.virgo.base;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 public class BaseActivity extends FragmentActivity {
 
+	public void toast(String msg)
+	{
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+	}
+	
+	public void forward(Class<?> classObj)
+	{
+		Intent intent = new Intent();
+		intent.setClass(this, classObj);
+		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		this.startActivity(intent);
+	}
 }
---
+++ src/com/xiaoxiao/virgo/utils/LogUtil.java
@@ -0,0 +1,39 @@
+package com.xiaoxiao.virgo.utils;
+
+import android.util.Log;
+
+public class LogUtil {	
+	
+	public static Boolean DEBUG = true;
+	
+	public static void v(String tag,String msg)
+	{
+		if(DEBUG)
+		{
+			Log.v(tag, msg);
+		}
+	}
+	
+	public static void d(String tag,String msg)
+	{
+		if(DEBUG)
+		{
+			Log.d(tag, msg);
+		}
+	}
+	
+	public static void i(String tag,String msg)
+	{
+		Log.i(tag, msg);
+	}
+	
+	public static void w(String tag,String msg)
+	{
+		Log.w(tag, msg);
+	}
+	
+	public static void e(String tag,String msg)
+	{
+		Log.e(tag, msg);
+	}
+}
