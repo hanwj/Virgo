699337fbc6d4cb7acc25a0928fc4f6c69bf82e7e
hanwj <hanwj>
2015-11-17 18:08:01 +0800

    cartoon_detail

--- src/com/xiaoxiao/fragment/HomePageFragment.java
+++ src/com/xiaoxiao/fragment/HomePageFragment.java
@@ -73,7 +73,7 @@ public class HomePageFragment extends Fragment {
 				@Override
 				public void onClick(View v) {
 					// TODO Auto-generated method stub
-					Toast.makeText(getActivity(), "viewPager "+position, Toast.LENGTH_SHORT).show();
+					//Toast.makeText(getActivity(), "viewPager "+position, Toast.LENGTH_SHORT).show();
 					Intent intent = new Intent(getActivity(),CartoonDetailPageActivity.class);
 					getActivity().startActivity(intent);
 				}
--- src/com/xiaoxiao/virgo/CartoonDetailPageActivity.java
+++ src/com/xiaoxiao/virgo/CartoonDetailPageActivity.java
@@ -4,17 +4,26 @@ import com.xiaoxiao.view.MyGridView;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.TextUtils;
+import android.transition.Visibility;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CartoonDetailPageActivity extends Activity implements OnClickListener{
 	
 	private int[] mChapters = {1,2,3,4,5,6,7,8,10};
+	private TextView introText;
+	private ImageView moreView;
+	private Boolean introFlag = false; //标记介绍是否展开
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -27,6 +36,22 @@ public class CartoonDetailPageActivity extends Activity implements OnClickListen
 	
 	private void initView()
 	{
+		String name = "海贼王";
+		//title
+		((TextView)findViewById(R.id.id_title)).setText(name);
+		
+		TextView nameText = (TextView)findViewById(R.id.id_name);
+		String nameHtml = "<font color='red'>[完]</font><font color='black'>" + name +"</font>";
+		nameText.setText(Html.fromHtml(nameHtml));
+		((TextView)findViewById(R.id.id_autor)).setText("作者：蔡潇潇");
+		
+		//intro
+		int maxLines = 2;
+		introText = (TextView)findViewById(R.id.id_introduction);
+		introText.setText("介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍");
+		int lines = introText.getLineCount();
+		moreView = (ImageView)findViewById(R.id.id_more_btn);
+		new MyOpenTask().execute(100);
 		//显示章节
 		MyGridView gridView = (MyGridView)findViewById(R.id.id_chapter_container);
 		gridView.setAdapter(new ButtonAdapter(this));
@@ -42,6 +67,30 @@ public class CartoonDetailPageActivity extends Activity implements OnClickListen
 		if(id == R.id.id_backbtn)
 		{
 			finish();
+		}
+		else if(id == R.id.id_downloadbtn)
+		{
+			//下载
+		}
+		else if(id == R.id.id_giftbtn)
+		{
+			//礼物
+		}
+		else if(id == R.id.id_more_btn)
+		{
+			if (introFlag)
+			{
+				introText.setEllipsize(TextUtils.TruncateAt.END);
+				introText.setMaxLines(2);
+				moreView.setRotation(0);
+			}
+			else
+			{
+				introText.setEllipsize(null);
+				introText.setMaxLines(Integer.MAX_VALUE);
+				moreView.setRotation(180);
+			}
+			introFlag = !introFlag;
 		}
 	}
 	
@@ -81,6 +130,35 @@ public class CartoonDetailPageActivity extends Activity implements OnClickListen
 			view.setText("1话");
 			
 			return view;
+		}
+		
+	}
+	
+	private class MyOpenTask extends AsyncTask<Integer, Integer, Integer>
+	{
+
+		@Override
+		protected Integer doInBackground(Integer... params) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			// TODO Auto-generated method stub
+			int lines = introText.getLineCount();
+			if(lines > 2)
+			{
+				introText.setMaxLines(2);
+				introText.setEllipsize(TextUtils.TruncateAt.END);
+				moreView.setVisibility(View.VISIBLE);
+				moreView.setOnClickListener(CartoonDetailPageActivity.this);
+			}
+			else
+			{
+				moreView.setVisibility(View.GONE);
+			}
+			super.onPostExecute(result);
 		}
 		
 	}
