7df2d8bcb79de67ec0fd76757a2dee0714c2109d
hanwj <hanwj>
2015-11-18 19:10:34 +0800

    fixbug

---
+++ src/com/xiaoxiao/fragment/RankPageFragment.java
@@ -0,0 +1,15 @@
+package com.xiaoxiao.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+public class RankPageFragment extends Fragment {
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+}
